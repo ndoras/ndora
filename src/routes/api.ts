96f28dffac008db6207168ac9ef212a38eb5c592
wangruirui <wangruirui@ruijie.com.cn>
2022-09-04 13:31:38 +0800

    init

---
+++ src/routes/api.ts
@@ -0,0 +1,13 @@
+import Router from '@koa/router'
+// import oauth from './oauth'
+// import article from './article'
+
+const router = new Router({
+  prefix: '/api',
+})
+
+// api list
+// router.use(oauth.routes(), oauth.allowedMethods())
+// router.use(article.routes(), article.allowedMethods())
+
+export default router
---
+++ src/routes/index.ts
@@ -0,0 +1,13 @@
+import Router from '@koa/router'
+
+import api from './api'
+// import logout from './logout'
+
+const router = new Router()
+
+router.use(api.routes()).use(api.allowedMethods())
+
+// router.use(logout.routes())
+// router.use(logout.allowedMethods())
+
+export default router
